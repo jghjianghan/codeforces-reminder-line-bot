b754222e28aa2359244dc211450347e71fdf2620
Jiang Han <jianghan99@gmail.com>
2022-02-03 00:18:14 +0700

    Initial commit from base example

---
+++ app.js
@@ -0,0 +1,33 @@
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+};
+const client = new line.Client(config);
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendStatus(404);
+});
+app.get('/', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(mainProgram))
+        .then((result) => res.json(result))
+        .catch((error) => {
+            console.error(`Promise error ${error}`);
+        });
+});
+
+const mainProgram = (event) => {
+    if (event.type !== 'message' || event.message.type !== 'text') { //jika user tidak mengirimkan pesan berupa teks (bukan gambar, lokasi, atau sejenisnya)
+        return Promise.resolve(null); //abaikan pesan
+    }
+    return client.replyMessage(event.replyToken, { type: 'text', text: 'Hello, world' }); //balas dengan pesan "Hello, world"
+}
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`app running in port ${port}...`);
+})
